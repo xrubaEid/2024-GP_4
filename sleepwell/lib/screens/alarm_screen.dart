ac7335b84b235ca5f819bf810ddb456c4d0e964a
Remak16 <143387825+Remak16@users.noreply.github.com>
2024-03-18 02:27:22 +0300

    clock

--- sleepwell/lib/screens/alarm_screen.dart
+++ sleepwell/lib/screens/alarm_screen.dart
@@ -1,10 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 
-=======
->>>>>>> a5772954afbe167ad5addf62c64194358466bcd6
 class AlarmScreen extends StatefulWidget {
-static String RouteScreen ='alarm_screen';
+  static String RouteScreen = 'alarm_screen';
 
   const AlarmScreen({super.key});
 
@@ -16,15 +14,11 @@ class _AlarmScreenState extends State<AlarmScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-
-<<<<<<< HEAD
-      body:  Center(child:Text("Alarms")),
-=======
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Center(child:Center(child: Text("Wake Up time "))),
-          /*DigitalClock(
+          Center(child: Center(child: Text("Wake Up time "))),
+          DigitalClock(
             digitAnimationStyle: Curves.easeInOut,
             is24HourTimeFormat: false,
             areaDecoration: BoxDecoration(
@@ -34,10 +28,9 @@ class _AlarmScreenState extends State<AlarmScreen> {
               color: Colors.blueGry,
               fontSize: 50,
             ),
-          ),*/
+          ),
         ],
       ),
->>>>>>> a5772954afbe167ad5addf62c64194358466bcd6
     );
   }
 }
