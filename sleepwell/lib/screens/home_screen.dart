5b276eec089048317ad2273ee0224eca946a25c0
Remak16 <143387825+Remak16@users.noreply.github.com>
2024-03-21 16:41:11 +0300

    .

--- sleepwell/lib/screens/home_screen.dart
+++ sleepwell/lib/screens/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:sleepwell/screens/alarm_screen.dart';
 import 'package:sleepwell/screens/profile_screen.dart';
 
 class MyHomePage extends StatefulWidget {
-   static String RouteScreen ='home_screen';
+  static String RouteScreen = 'home_screen';
   const MyHomePage({super.key});
 
   @override
@@ -12,75 +12,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _auth= FirebaseAuth.instance;
-late User signInUser ;
- 
- @override
+  final _auth = FirebaseAuth.instance;
+  late User signInUser;
+
+  @override
   void initState() {
     super.initState();
     getCurrentUser();
   }
-  void getCurrentUser(){
-    // check is the user sign up or not ? 
-   try {
-    final user =_auth.currentUser;
-   // if rutern 0 no user found if not will rutern the email and the password
-   if (user !=null){
-    signInUser= user;
-    // should be deleted now just for testing
-    print(signInUser.email);
-   } 
-   } catch (e) {
-     print(e);
-   }
+
+  void getCurrentUser() {
+    // check is the user sign up or not ?
+    try {
+      final user = _auth.currentUser;
+      // if rutern 0 no user found if not will rutern the email and the password
+      if (user != null) {
+        signInUser = user;
+        // should be deleted now just for testing
+        print(signInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
   }
-  int index =2;
-  final pages =[
+
+  int index = 2;
+  final pages = [
     ProfileScreen(),
     const Center(child: Text('Hello  statistic  ')),
     AlarmScreen(),
     const Center(child: Text('Hello Dashboard')),
   ];
-  
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          backgroundColor:const Color.fromARGB(255, 0, 74, 173),
-          title: Text(''),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 0, 74, 173),
+        title: Text(''),
+      ),
       bottomNavigationBar: NavigationBar(
         selectedIndex: index,
-        onDestinationSelected: (index) => setState(()=> this.index = index) ,
-        backgroundColor:Color(0xFF040E3B),
+        onDestinationSelected: (index) => setState(() => this.index = index),
+        backgroundColor: Color(0xFF040E3B),
         height: 70,
         destinations: const [
           NavigationDestination(
-            icon:Icon( Icons.person_outlined ,color: Colors.white),
-            selectedIcon:Icon( Icons.person ,color: Colors.white), 
-            label:'profile',
-            ),
-             NavigationDestination(
-            icon:Icon( Icons.align_vertical_bottom_outlined ,color: Colors.white),
-            selectedIcon:Icon( Icons.align_vertical_bottom,color: Colors.white ), 
-            label:'Statistic',
-            ),
-             NavigationDestination(
-            icon:Icon( Icons.access_alarm_outlined,color: Colors.white ),
-            selectedIcon:Icon( Icons.access_alarm ,color: Colors.white), 
-            label:'Alarm',
-            ),
-           NavigationDestination(
-  icon: Icon(Icons.dashboard_customize_outlined, color: Colors.white),
-  selectedIcon: Icon(Icons.dashboard_customize, color: Colors.white),
-  label: 'Dashboard',
-),
+            icon: Icon(Icons.person_outlined, color: Colors.white),
+            selectedIcon: Icon(Icons.person, color: Colors.white),
+            label: 'profile',
+          ),
+          NavigationDestination(
+            icon:
+                Icon(Icons.align_vertical_bottom_outlined, color: Colors.white),
+            selectedIcon:
+                Icon(Icons.align_vertical_bottom, color: Colors.white),
+            label: 'Statistic',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.access_alarm_outlined, color: Colors.white),
+            selectedIcon: Icon(Icons.access_alarm, color: Colors.white),
+            label: 'Alarm',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.dashboard_customize_outlined, color: Colors.white),
+            selectedIcon: Icon(Icons.dashboard_customize, color: Colors.white),
+            label: 'Dashboard',
+          ),
         ],
       ),
-     
-     body:pages[index],
-    
-   
-      );
-  } 
+      body: pages[index],
+    );
+  }
 }
